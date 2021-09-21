b01b885aa45751f50626c4b7350c543787dc9cdf
kauangouveia <kauangouveia64@gmail.com>
2021-09-21 11:14:23 -0300

    main

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import { Header } from "./styles";
+import logo from "../assets/logo.png";
+
+function HeaderHome() {
+  return (
+    <Header>
+      <div>
+        <div>
+          <span>Entrar</span>
+          <span>Quem Somos</span>
+          <span>Serviços</span>
+          <span>Inicio          </span>
+        </div>
+        <img src={logo} alt="logo" />
+      </div>
+    </Header>
+  );
+}
+
+export default HeaderHome;
