a183239319d35b08cba89720e4da48d4265f90c0
Helder Siqueira <helder.erik.he@gmail.com>
2021-09-22 11:28:42 -0300

    Criação da listagem de prestadores

--- src/pages/ListServices/index.js
+++ src/pages/ListServices/index.js
@@ -1,11 +1,45 @@
 import Header from "../../components/Header"
 import {Container} from "./styles"
+import {SearchContainer, ContainerFilter, CardContainer, Profile} from "./styles"
+import search from "../../assets/search.png";
 
 function ListServices(){
     return(
+    <>    
         <Container>
             <Header/>
+            <SearchContainer>
+            <div className="InputWithImg">
+            <form>
+              <label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="O que você precisa?"
+                />
+              </label>
+            </form>
+            <img src={search} alt="" />
+          </div>
+            </SearchContainer>
+            <ContainerFilter>
+                <div className="Filters">
+                    
+                </div>
+                <div className="Results">
+
+                </div>
+            </ContainerFilter>
+            <CardContainer>
+                <Profile>
+
+                </Profile>
+                <Contact>
+                    
+                </Contact>
+            </CardContainer>
         </Container>
+    </>
     );
 }
 
--- src/pages/ListServices/styles.js
+++ src/pages/ListServices/styles.js
@@ -2,8 +2,88 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  background-color: red;
   width: 100vw;
   height: 1268px;
   display: flex;
   flex-direction: column;
 `;
+
+export const SearchContainer = styled.div`
+  background-color: green;
+  width: 1360px;
+  height: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 65px;
+  overflow: hidden;
+
+  .InputWithImg {
+      width: 100%;
+      height: 150px;
+      display: flex;
+
+      justify-content: center;
+      input {
+        margin-top: 20px;
+        width: 500px;
+        border-radius: 5px;
+        height: 40px;
+        font-size: 20px;
+        border: solid 0.5px #dadada;
+        padding: 10px;
+      }
+      input::placeholder {
+        color: #5679e0;
+      }
+      img {
+        margin-top: 20px;
+        width: 40px;
+        height: 40px;
+        margin-left: 15px;
+      }
+      img:hover{
+        cursor: pointer;
+      }
+    }
+`;
+
+export const ContainerFilter = styled.div`
+  background-color: aliceblue;
+  width: 1360px;
+  min-height: 100px;
+  margin-left: auto;
+  margin-right: auto;
+
+    .Filters{
+      background-color: purple;
+      width: 1360px;
+      height: 100px;
+    }
+
+    .Results{
+      background-color: yellowgreen;
+      width: 360px;
+      height: 100px;
+    }
+`;
+
+export const CardContainer = styled.div`
+  background-color: yellow;
+  width: 1390px;
+  min-height: 250px;
+  height: auto;
+  overflow-y: scroll;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Profile = styled.div`
+
+`;
+
+export const Contact = styled.div`
+
+`;
+
+
