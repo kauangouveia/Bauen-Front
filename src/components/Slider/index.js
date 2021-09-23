f28501644d583746a3e3a390c221173e9b6467ab
kauangouveia <kauangouveia64@gmail.com>
2021-09-23 11:24:17 -0300

    finalizando slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,32 +1,40 @@
+import { SlideContainer } from "./Styles";
+
 import React, { Component } from "react";
 import Carousel from "react-elastic-carousel";
 import Reform from "../../assets/reform.jpg";
 import reforme from "../../assets/reforme.jpg";
 import encanador from "../../assets/encanador.png";
-import {CardImagesServices} from "./Styles"
-class App extends Component {
+// import { ContainerSliderCard } from "./Styles";
+class Slider extends Component {
   state = {
     items: [
-      { id: 1, title: <img src={Reform} alt="serviço prestado"/>},
-      { id: 2, title: <img src={reforme} alt="serviço prestado"/>},
-      { id: 3, title: <img src={encanador} alt="serviço prestado"/>},
-      { id: 4, title: <img src={Reform} alt="serviço prestado"/>},
-      { id: 5, title: <img src={Reform} alt="serviço prestado"/>},
-      { id: 6, title: <img src={Reform} alt="serviço prestado"/>},
-      { id: 7, title: <img src={Reform} alt="serviço prestado"/>},
+      { id: 1, title: <img src={Reform} alt="serviço prestado" /> },
+      { id: 2, title: <img src={reforme} alt="serviço prestado" /> },
+      { id: 3, title: <img src={encanador} alt="serviço prestado" /> },
+      { id: 4, title: <img src={Reform} alt="serviço prestado" /> },
+      { id: 5, title: <img src={Reform} alt="serviço prestado" /> },
+      { id: 6, title: <img src={Reform} alt="serviço prestado" /> },
+      { id: 7, title: <img src={Reform} alt="serviço prestado" /> },
     ],
   };
 
   render() {
     const { items } = this.state;
     return (
-      <Carousel itemsToShow={3}>
-        {items.map((item) => (
-          <CardImagesServices key={item.id}>{item.title}</CardImagesServices>
-        ))}
-      </Carousel>
+      <SlideContainer>
+        <div className="SliderContainer">
+          <Carousel itemsToShow={3}>
+            {items.map((item) => (
+              <div className="CardImagesServices" key={item.id}>
+                {item.title}
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </SlideContainer>
     );
   }
 }
 
-export default App;
+export default Slider;
