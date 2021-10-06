9b47b804d958121a33d111c72b66a36688946825
kauangouveia <kauangouveia64@gmail.com>
2021-10-06 11:18:53 -0300

    chore : pasta

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -5,8 +5,10 @@ import clip from "../../assets/clip.svg";
 import { useState } from "react";
 import io from "socket.io-client";
 
-const socket = io('http://localhost:3334')
-socket.on('connect', () => console.log('[IO] Connect => A new connection has been established'))
+const socket = io("http://localhost:8080/");
+socket.on("connect", () =>
+  console.log("[IO] Connect => A new connection has been established")
+);
 
 function Chat() {
   const [message, updateMessage] = useState("");
@@ -16,10 +18,13 @@ function Chat() {
   const handleFormOnSubmit = (event) => {
     event.preventDefault();
     if (message.trim()) {
-      updateMessages([...messages, {
-        id : 1,
-        message
-      }])
+      updateMessages([
+        ...messages,
+        {
+          id: 1,
+          message,
+        },
+      ]);
       updateMessage("");
     }
   };
